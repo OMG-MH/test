43c861a25a50dda5672318b8a03d02553c3cb11b
OMG-MH <86635694+OMG-MH@users.noreply.github.com>
2025-08-11 00:09:35 +0300

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-# main.py
+import os
 import telebot
+from flask import Flask, request
 from config import BOT_TOKEN
 from handlers import register_handlers
 
 bot = telebot.TeleBot(BOT_TOKEN)
 register_handlers(bot)
 
-print("✅ البوت يعمل الآن ...")
-bot.infinity_polling()
+app = Flask(name)
+
+@app.route('/')
+def index():
+    return "Bot is running"
+
+@app.route(f"/{BOT_TOKEN}", methods=['POST'])
+def receive_update():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "OK", 200
+
+if name == "main":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 # main.py
 # from telegram.ext import Application
